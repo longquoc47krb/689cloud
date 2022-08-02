767c2093c2e38418d891ca9aea27b6fa88c49aef
LongQ <longquoc47krb@gmail.com>
2022-08-02 19:17:54 +0700

    Edit DetailContent.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { Button } from "antd";
 import Header from "./components/Header";
-import { AccountSettings } from "./components/Modal";
-import logo from "./logo.svg";
+import { AccountSettings, DetailContent } from "./components/Modal";
+import logo from "../src/assets/images.png";
 function App() {
   return (
-    <div className='bg-[#E2E2E2]'>
+    <div className='bg-[#E2E2E2] h-[625px] w-[1366px]'>
       <Header />
-      <AccountSettings />
+      {/* <AccountSettings /> */}
+      <DetailContent
+        photo='https://picsum.photos/536/380'
+        visible={true}
+        title='Title'
+        author='Author'
+        gen='Gen'
+        publisher='Publisher'
+        group='Group'
+        genre='Genre'
+      />
     </div>
   );
 }
