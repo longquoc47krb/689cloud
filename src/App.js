65a6aea0ffeaa6b4ba98850467f2c2622b45e427
longquoc47krb <longquoc47krb>
2022-08-09 16:56:28 +0700

    Implement drag & drop feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "antd/dist/antd.min.css";
+import Table from "./pages/Table";
+import Todo from "./pages/Todo";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./layout";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AppLayout />}>
+          <Route path='/todo' element={<Todo />} />
+          <Route path='/table' element={<Table />} />
+          <Route path='/' element={<Navigate to='/table' replace />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
