d883ed346ee65ae26301965ef4eab3238fa22fc4
longquoc47krb <longquoc47krb>
2022-08-11 06:48:48 +0700

    Ajdusting validate.js

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -57,10 +57,12 @@ const validateSearchBox = Yup.object().shape({
   // searchFields: Yup.array().string(),
   label: Yup.string().required("Required"),
   operation: Yup.string().required("Required"),
-  matchValues: Yup.object().shape({
-    key: Yup.string().required("Required"),
-    value: Yup.string().required("Required"),
-  }),
+  matchValues: Yup.array().of(
+    Yup.object().shape({
+      key: Yup.string().required("Required"),
+      value: Yup.string().required("Required"),
+    })
+  ),
 });
 
 export {
