f950b30023bce7bd90759c8a5fac0c70d0d19eeb
longquoc47krb <longquoc47krb>
2022-08-12 18:49:33 +0700

    Import Text Editor component to EmailContent.jsx. EmailContent includes email subject and email body.

--- src/pages/Messages/Email/EmailContent.jsx
+++ src/pages/Messages/Email/EmailContent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Input } from "antd";
-import { EditorComponent } from "../../../components/Editor";
+import { TextEditor } from "../../../components/Editor";
 
 const EmailContent = () => {
   return (
@@ -8,7 +8,7 @@ const EmailContent = () => {
       <h1>Japanes Email Subject</h1>
       <Input />
       <h1>Japanes Email Body</h1>
-      <EditorComponent />
+      <TextEditor />
     </div>
   );
 };
